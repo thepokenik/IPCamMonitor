ad0ba4b02fd6c51af1582395678aa73035006d2b
thepokenik <nikolaspedro.np@gmail.com>
2025-02-25 16:08:32 -0300

    feat: Wrap App component with CameraProvider for camera state management

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./main.css";
+
 import { ThemeProvider } from "./components/theme-provider";
+import { CameraProvider } from "./contexts/CameraContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <App />
+            <CameraProvider>
+                <App />
+            </CameraProvider>
         </ThemeProvider>
     </React.StrictMode>,
 );
