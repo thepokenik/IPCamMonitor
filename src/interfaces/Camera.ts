9b3f0bb997e6d49d8f8b3a2e419e018c771afda7
thepokenik <nikolaspedro.np@gmail.com>
2025-02-26 15:05:20 -0300

    refactor: Update Camera interface to include port

--- src/interfaces/Camera.ts
+++ src/interfaces/Camera.ts
@@ -1,12 +1,15 @@
 export default interface Camera {
     id: string
     status: CameraStatus
-    ip: string
-    file: File | null
+    ip: string | null
+    port: number | null
+    file: File | Blob | null
     preview: string | null
 }
 
 export enum CameraStatus {
     Idle = 'idle',
-    Streaming = 'streaming'
+    Streaming = 'streaming',
+    Loading = 'loading',
+    Error = 'error'
 }
